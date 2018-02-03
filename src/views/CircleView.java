a98b1033aca1ebe8fb95c3e225eab0bfe267f1cb
Jacob Hackett <jacobrhackett@gmail.com>
2018-02-03 12:09:34 -0500

    further generalizing circle view

--- src/views/CircleView.java
+++ src/views/CircleView.java
@@ -8,30 +8,12 @@ import views.styles.Colors;
 public class CircleView {
 
     private Circle circle;
-    private Circle insideCircle;
-    private Circle innerTimerCircle;
-    private Circle outerTimerCircle;
 
-    private int timerWidth = 4;
-
-    public void initCircles(Paint color, SimpleBooleanProperty time_visible) {
+    public Circle initCircle(Paint color, int radius, SimpleBooleanProperty time_visible) {
         circle = new Circle();
-        insideCircle = new Circle();
-        innerTimerCircle = new Circle();
-        outerTimerCircle = new Circle();
-
-        circle.setRadius(90);
-        circle.setFill(Colors.WHITE.getColor());
-
-        insideCircle.setRadius(80);
-        insideCircle.setFill(color);
-
-        innerTimerCircle.setRadius(70);
-        innerTimerCircle.setFill(insideCircle.getFill());
-        innerTimerCircle.visibleProperty().bind(time_visible);
-
-        outerTimerCircle.setRadius(innerTimerCircle.getRadius() + timerWidth);
-        outerTimerCircle.setFill(Colors.WHITE.getColor());
-        outerTimerCircle.visibleProperty().bind(time_visible);
+        circle.setRadius(radius);
+        circle.setFill(color);
+        circle.visibleProperty().bind(time_visible);
+        return circle;
     }
 }
